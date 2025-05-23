3cdec6e74ff837cec1669ec54b77fd7e206af0a4
Zulius Roolf <36889707+ZuliusRoolf@users.noreply.github.com>
2025-05-23 19:50:47 +0200

    chapter 3 challenge

--- GeoQuiz/app/build.gradle.kts
+++ GeoQuiz/app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.media3.common.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -6,7 +6,6 @@ import android.widget.Button
 import android.widget.Toast
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -18,7 +17,21 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val quizViewModel: QuizViewModel by viewModels()
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private val answers = arrayOfNulls<Int>(questionBank.size)
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +40,6 @@ class MainActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -44,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.nextButton.setOnClickListener {
-            quizViewModel.moveToNext()
+            currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
 
@@ -77,19 +88,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
-        val questionTextResId = quizViewModel.currentQuestionText
+        val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
+        updateButtonState()
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = quizViewModel.currentQuestionAnswer
+        val correctAnswer = questionBank[currentIndex].answer
 
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
         } else {
             R.string.incorrect_toast
         }
+        answers[currentIndex] = if (userAnswer == correctAnswer) {100} else {0}
+        updateButtonState()
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        gradeAnswers()
+    }
+
+    private fun updateButtonState() {
+        binding.trueButton.isEnabled = (answers[currentIndex] == null)
+        binding.falseButton.isEnabled = (answers[currentIndex] == null)
+    }
+
+    private fun gradeAnswers() {
+        var result: Int = 0
+        if (questionBank.size == 0) return
+        for (answer in answers){
+            if (answer == null) return
+            result += answer
+        }
+        result /= questionBank.size
+        Toast.makeText(this, "$result% correct!", Toast.LENGTH_SHORT).show()
     }
 }
