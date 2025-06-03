113aca141ee02fce3f06f569cc55d9720728616b
Zulius Roolf <36889707+ZuliusRoolf@users.noreply.github.com>
2025-06-03 18:22:59 +0200

    ch12 coroutines (async)

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -1,12 +1,29 @@
 package com.bignerdranch.android.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
+
+private val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
     init {
+        Log.d(TAG, "init starting")
+        viewModelScope.launch{
+            Log.d(TAG, "coroutine launched")
+            crimes += loadCrimes()
+            Log.d(TAG, "Loading crimes finished")
+        }
+    }
+
+    suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000)
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
@@ -14,7 +31,8 @@ class CrimeListViewModel : ViewModel() {
                 date = Date(),
                 isSolved = i % 2 == 0
             )
-            crimes += crime
+            result += crime
         }
+        return result
     }
 }
