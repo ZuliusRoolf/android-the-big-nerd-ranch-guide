0c9a6d454f2aa04dda5237decda5dc8357de833b
Zulius Roolf <36889707+ZuliusRoolf@users.noreply.github.com>
2025-06-07 20:35:41 +0200

    ch15 app bar

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val toolbar: MaterialToolbar = findViewById(R.id.topAppBar)
+        setSupportActionBar(toolbar)
     }
 }
