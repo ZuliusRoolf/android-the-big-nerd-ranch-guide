dfcf24cc77ba3289edb5264267ec27dbdde1a885
Zulius Roolf <36889707+ZuliusRoolf@users.noreply.github.com>
2025-06-21 20:59:40 +0200

    ch9 challenge add dependencies

--- CriminalIntent/app/build.gradle.kts
+++ CriminalIntent/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.fragment.testing)
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
--- CriminalIntent/app/src/androidTest/java/com/bignerdranch/android/criminalintent/CrimeDetailFragmentTest.kt
+++ CriminalIntent/app/src/androidTest/java/com/bignerdranch/android/criminalintent/CrimeDetailFragmentTest.kt
@@ -1,8 +1,18 @@
 package com.bignerdranch.android.criminalintent
 
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.junit.After
 import org.junit.Before
- class CrimeDetailFragmentTest {
+import org.junit.Test
+import java.util.UUID
+
+class CrimeDetailFragmentTest {
 
 @Before
  fun setUp() {}
